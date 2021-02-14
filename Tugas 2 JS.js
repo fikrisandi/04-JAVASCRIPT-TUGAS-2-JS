831e4e4120a0d678bf20d160af0826c2e4f6199f
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-02-14 15:43:57 +0700

    Tugas 2 JS

---
+++ Tugas 2 JS.js
@@ -0,0 +1,13 @@
+var umur = 25; //number
+var nama = "Sammy Trevor"; //string
+var boolean = true; //boolean
+var tidakada; //undifined
+var kosong = null; //null
+var AngkaTogel = [20, 21, 22, 23, 24]; //array
+
+console.log(umur);
+console.log(nama);
+console.log(boolean);
+console.log(tidakada);
+console.log(kosong);
+console.log(AngkaTogel);
